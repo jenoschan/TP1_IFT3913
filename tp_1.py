81730b87eb563c202ef8c0606fa29f7f19605a07
jen <jenny.diep@icloud.com>
2022-09-19 11:54:28 -0400

    issue: csv headers added more than once

--- tp_1.py
+++ tp_1.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import csv
 
 #class tp_1: that has a jls method that returns hello world
 class tp_1:
@@ -8,15 +9,22 @@ class tp_1:
     #method that prints hello world
     def jls(self, path):
 
-        #create a csv file
-        csv_file = open("tp_1.csv", "w")
-
         path_list = path.split("\\")
         packet_name = ".".join(path_list[1:-1])
         class_name = os.path.basename(path).split(".")[0]
 
-        #return a csv file with path, packet name and class name
-        csv_file.write(path + "," + packet_name + "," + class_name+"\n")
+        header_added = False
+
+        #return csv file with chemin du fichier, nom du paquet and nom de la classe as headers
+        with open('tp_1.csv', 'a+', newline='') as csvfile:
+            fieldnames = ['chemin du fichier', 'nom du paquet', 'nom de la classe']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            if header_added == False:
+                writer.writeheader()
+                header_added = True
+            writer.writerow({'chemin du fichier': path, 'nom du paquet': packet_name, 'nom de la classe': class_name})
+
+        print(path + "," + packet_name + "," + class_name+"\n")
 
     
     def nvloc(self, path):
