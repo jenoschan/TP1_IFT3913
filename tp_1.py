a56a2fa9ad65e3a2890a734fc1ca335a4028ceb6
Charbel-Machaalani <76147076+Charbel-Machaalani@users.noreply.github.com>
2022-09-20 16:36:39 -0400

    Add files via upload

--- tp_1.py
+++ tp_1.py
@@ -1,13 +1,46 @@
 import os
+from queue import Empty
 import sys
 import csv
+from os import listdir
+from os.path import isfile, join
+from os import walk
+
 
 #class tp_1: that has a jls method that returns hello world
 class tp_1:
 
     path = sys.argv[1]
+
+    def jls2(self, path):
+        root = path.split('\\')[-1]
+        content = []
+        for files in os.walk(path):
+            if len(files[2]) > 0:
+                for file in files[2]:
+                    content.append(files[0] + '\\' + file)
+
+        result = "chemin du ficher,nom du paquet,nom de la classe\n"
+        for item in content:
+            toAdd = item + ','
+            candidate = item.split('\\')
+            while candidate[0] != root:
+                candidate.pop(0)
+            candidate.pop(0)
+            while len(candidate) > 1:
+                if len(candidate) > 2:
+                    toAdd += candidate.pop(0) + '.'
+                else: toAdd += candidate.pop(0)
+            toAdd += ',' + candidate.pop(0)
+            result += toAdd + '\n'
+
+        with open("tp_1.csv", "w") as file:
+            file.write(result)
+            file.close()
+
+
     #method that prints hello world
-    def jls(self, path):
+    def writeFile(self, path):
 
         path_list = path.split("\\")
         packet_name = ".".join(path_list[1:-1])
@@ -48,5 +81,5 @@ class tp_1:
 if __name__ == "__main__":
     tp = tp_1()
 
-    tp.jls(tp.path)
-    tp.nvloc(tp.path)
+    tp.jls2(tp.path)
+    #tp.nvloc(tp.path)
