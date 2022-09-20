51a9e1379244e9f7a6c1b277c354371cfa5320d0
Charbel-Machaalani <76147076+Charbel-Machaalani@users.noreply.github.com>
2022-09-20 17:18:35 -0400

    Add files via upload

---
+++ Nvloc.py
@@ -0,0 +1,21 @@
+import sys
+
+class Nvloc:
+
+    path = sys.argv[1]
+
+    def nvloc(self, path):
+    #open file and read it
+        source_file = open(path, "r")
+        #count the amount of non-empty lines
+        count = 0
+        for line in source_file:
+            if line.strip():
+                count += 1
+        print(count)
+
+
+if __name__ == "__main__":
+    instance = Nvloc()
+
+    instance.nvloc(instance.path)
