c903d432b2f872b370fa54950dc747b22088e62f
Charbel-Machaalani <76147076+Charbel-Machaalani@users.noreply.github.com>
2022-09-28 18:05:46 -0400

    Add files via upload

--- jls.py
+++ jls.py
@@ -1,49 +1,46 @@
-import pathlib as pl
 import os
+from queue import Empty
 import sys
 import csv
+from os import listdir
+from os.path import isfile, join
+from os import walk
 
+
+#class tp_1: that has a jls method that returns hello world
 class jls:
-    def jls(self):
-        path = sys.argv[1].replace('"', '')
 
-        if '\\' and '"' in path:
-            path = path.replace('\\', '/')
+    path = sys.argv[1]
 
-        path_list = path.split("/")
-        packet_name = ".".join(path_list[1:-1])
-        class_name = os.path.basename(path).split(".")[0]
+    def jls(self, path):
+        root = path.split('\\')[-1]
+        content = []
+        for files in os.walk(path):
+            if len(files[2]) > 0:
+                for file in files[2]:
+                    content.append(files[0] + '\\' + file)
 
-        #return csv file with chemin du fichier, nom du paquet and nom de la classe as headers
-        with open('tp_1.csv', 'a', newline='') as csvfile:
-            fieldnames = ['chemin du fichier', 'nom du paquet', 'nom de la classe']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            #if file is empty, write headers
-            if os.stat('tp_1.csv').st_size == 0:
-                writer.writeheader()
-                writer.writerow({'chemin du fichier': path, 'nom du paquet': packet_name, 'nom de la classe': class_name})
-            else:
-                writer.writerow({'chemin du fichier': path, 'nom du paquet': packet_name, 'nom de la classe': class_name})
-        
-        #print(path + "," + packet_name + "," + class_name+"\n")
+        result = "chemin du ficher,nom du paquet,nom de la classe\n"
+        for item in content:
+            toAdd = item + ','
+            candidate = item.split('\\')
+            while candidate[0] != root:
+                candidate.pop(0)
+            candidate.pop(0)
+            while len(candidate) > 1:
+                if len(candidate) > 2:
+                    toAdd += candidate.pop(0) + '.'
+                else: toAdd += candidate.pop(0)
+            toAdd += ',' + candidate.pop(0)
+            print(toAdd)
+            result += toAdd + '\n'
 
-        #if path doesnt exist
-        if not pl.Path(path).exists():
-            print("Path is in a valid format, but doesn't exist on this computer")
-            return
+        with open("tp_1.csv", "w") as file:
+            file.write(result)
+            file.close()
 
-        for root, dirs, files in os.walk(path):
-            level = root.replace(path, '').count(os.sep)
-            #print directories
-            if level == 0:
-                print(root)
-            #TODO : fix formatting
-            indent = ' ' * 4 * (level) + "|__"
-            print('{}{}/'.format(indent, os.path.basename(root)))
-            #print files
-            subindent = ' ' * 4 * (level) + "|" + " " * 4 + "|__"
-            for f in files:
-                print('{}{}'.format(subindent, f))
 
-if __name__ == '__main__':
-    jls.jls(sys.argv[1])
+if __name__ == "__main__":
+    Jls = jls()
+
+    Jls.jls(Jls.path)
