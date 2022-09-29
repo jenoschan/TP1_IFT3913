04c544ece40d15e4deaa9b37915f0a329a60d527
jen <jenny.diep@icloud.com>
2022-09-29 15:15:56 -0400

    nvloc now working!

--- Nvloc.py
+++ Nvloc.py
@@ -7,8 +7,8 @@ class nvloc:
     path = sys.argv[1]
 
     def nvloc(self, path):
-        #get class name from path
-        class_name = path.split('/')[-1][:-5]
+        #get file name from path 
+        file_name = pl.Path(path).name
         #open file and read it
         source_file = open(path, "r")
         #count the amount of non-empty lines
@@ -29,18 +29,16 @@ class nvloc:
                 #add count to corresponding class
                 for i in range(len(df)):
                     #get class name from path
-                    if df["nom de la classe"][i] == class_name:
+                    if df["nom de la classe"][i] == file_name:
                         df["NVLOC"][i] = count	
                         df.at[i, "NVLOC"] = count
-                        #TODO not working
                 #save changes to csv file
                 df.to_csv("tp_1.csv", index=False)
             else:
                 df = pd.read_csv("tp_1.csv")
                 for i in range(len(df)):
-                    if df["nom de la classe"][i] == class_name:
+                    if df["nom de la classe"][i] == file_name:
                         df.at[i, "NVLOC"] = count
-                        print(i)
 
 
 if __name__ == "__main__":
