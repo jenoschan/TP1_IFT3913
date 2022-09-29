92bd99e3e44879a1359f70c1f642661a30ba31e0
jen <jenny.diep@icloud.com>
2022-09-29 15:13:15 -0400

    csv issues

--- Nvloc.py
+++ Nvloc.py
@@ -19,28 +19,28 @@ class nvloc:
         #if create tp_1.csv if it doesnt exist
         if not pl.Path("tp_1.csv").exists():
             print("tp_1.csv does not exist, create it using jls.py")
-
-        #if NVLOC column is not in the csv file, create it and add the count to corresponding class
-        if "NVLOC" not in open("tp_1.csv").read():
-            #make copy of csv file
-            df = pd.read_csv("tp_1.csv")
-            #add NVLOC column
-            df["NVLOC"] = ""
-            #add count to corresponding class
-            for i in range(len(df)):
-                #get class name from path
-                if df["nom de la classe"][i] == class_name:
-                    df["NVLOC"][i] = count	
-                    df.at[i, "NVLOC"] = count
-                    #TODO not working
-            #save changes to csv file
-            df.to_csv("tp_1.csv", index=False)
         else:
-            df = pd.read_csv("tp_1.csv")
-            for i in range(len(df)):
-                if df["nom de la classe"][i] == class_name:
-                    df.at[i, "NVLOC"] = count
-                    print(i)
+            #if NVLOC column is not in the csv file, create it and add the count to corresponding class
+            if "NVLOC" not in open("tp_1.csv").read():
+                #make copy of csv file
+                df = pd.read_csv("tp_1.csv")
+                #add NVLOC column
+                df["NVLOC"] = ""
+                #add count to corresponding class
+                for i in range(len(df)):
+                    #get class name from path
+                    if df["nom de la classe"][i] == class_name:
+                        df["NVLOC"][i] = count	
+                        df.at[i, "NVLOC"] = count
+                        #TODO not working
+                #save changes to csv file
+                df.to_csv("tp_1.csv", index=False)
+            else:
+                df = pd.read_csv("tp_1.csv")
+                for i in range(len(df)):
+                    if df["nom de la classe"][i] == class_name:
+                        df.at[i, "NVLOC"] = count
+                        print(i)
 
 
 if __name__ == "__main__":
